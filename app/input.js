1f0b786dd75604f2438813a37b3906f8c814eb5d
Sean <shonum@gmail.com>
2016-11-15 08:51:16 -0800

    Tidy up HTML

--- app/input.js
+++ app/input.js
@@ -44,7 +44,12 @@
     $scope.roofs = [];
 
     $scope.financials = {
-      display: false
+      display: false,
+      chart: {
+        labels: ['2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026'],
+        cashflowData: []
+      }
+
     };
 
     $scope.calculate = function() {
@@ -68,6 +73,14 @@
         $scope.financials.totalWattage = roofs.reduce((a, b)=>{return a + b.totalWattage}, 0);
         $scope.financials.annualAC = results.reduce((a, b)=>{return a + b.ac_annual}, 0);
         $scope.financials.display = true;
+        $scope.financials.chart.cashflowData.push(0 - ($scope.financials.totalWattage * 3.5));
+        for (let i = 1; i < 10; i++) {
+          let prevCF = $scope.financials.chart.cashflowData[i-1];
+          let currCF = prevCF + (0.14 * $scope.financials.annualAC);
+          $scope.financials.chart.cashflowData.push(currCF);
+
+        }
+        console.log($scope.financials.chart.cashflowData);
       });
     };
 
